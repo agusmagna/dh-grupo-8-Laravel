1f5a08d6f42c70af19fae7f010a20a30601a41b0
Agustina Magnalardo <agusmagna@MacBook-Pro-de-AgustinaMagna.local>
2019-07-30 23:54:40 -0300

    Migrations de cápsulas y máquinas

---
+++ database/migrations/2019_07_31_024814_create_machines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMachinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->decimal('price',8,2);
+            $table->string('color');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('machines');
+    }
+}
