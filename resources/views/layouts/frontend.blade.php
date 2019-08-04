769bc6791cf04d3d235bbb36d41d4de5fb65e41e
Agustina Magnalardo <agusmagna@MacBook-Pro-de-AgustinaMagna.local>
2019-08-04 19:45:15 -0300

    Layout frontend

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Tigout') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    @yield('headScript')
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet">
+    <link href="{{ asset('css/stylesLayoutFrontend.css') }}" rel="stylesheet">
+    @yield('css')
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light  shadow-sm">
+            <div class="container">
+              <header>
+                <a class="navbar-brand logo" href="{{ url('/index') }}">
+                    <img src="{{ asset('images/logo_sin_fondo_blanco.png')}}" alt="Logo">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                      @if(strpos($_SERVER['SCRIPT_FILENAME'],'index.php'))
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="#quienes-somos">Conocenos</a></li>
+                        <li><a href="#productos">Productos</a></li>
+                        <li><a href="contacto.php">Contacto</a></li>
+                      @else
+                      <li><a href="index.php">Inicio</a></li>
+                      <li><a href="index.php#quienes-somos">Conocenos</a></li>
+                      <li><a href="productos.php">Productos</a></li>
+                      <li><a href="contacto.php">Contacto</a></li>
+                      @endif
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li>
+                                <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li>
+                                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </header>
+      <div class="content">
+        @yield('content')
+      </div>
+
+      <footer>
+      <div class="logo-footer">
+        <img src="{{ asset('images/logo_sin_fondo_blanco.png')}}" alt="Logo">
+      </div>
+      <div class="footer-navegacion">
+        <div class="footer-capsulas">
+          @for ($i=0; $i < 5; $i++)
+            @if ($i%2==0)
+              <div class="footer-capsula1"><img src="{{ asset('images/contorno_capsula.png')}}" alt=""></div>;
+            @else
+              <div class="footer-capsula2"><img src="{{asset('images/contorno_capsula.png')}}" alt=""></div>;
+            @endif
+          @endfor
+        </div>
+        <ul>
+          @if(strpos($_SERVER['SCRIPT_FILENAME'],'index.php'))
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="#quienes-somos">Conocenos</a></li>
+            <li><a href="#productos">Productos</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+          @else
+          <li><a href="index.php">Inicio</a></li>
+          <li><a href="index.php#quienes-somos">Conocenos</a></li>
+          <li><a href="productos.php">Productos</a></li>
+          <li><a href="contacto.php">Contacto</a></li>
+          @endif
+        </ul>
+        <div class="footer-capsulas">
+          @for ($i=0; $i < 5; $i++)
+            @if ($i%2==0)
+              <div class="footer-capsula1"><img src="{{ asset('images/contorno_capsula.png')}}" alt=""></div>;
+            @else
+              <div class="footer-capsula2"><img src="{{asset('images/contorno_capsula.png')}}" alt=""></div>;
+            @endif
+          @endfor
+        </div>
+      </div>
+      <div class="redes-sociales">
+        <h3>Seguinos en</h3>
+        <div class="iconos">
+          <a href="https://es-la.facebook.com/tigout.AR/"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/tigout.ar/"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+    </footer>
+      <!-- scripts finales -->
+
+      @yield('finalScript')
+
+    </div>
+</body>
+</html>
