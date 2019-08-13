cb76a0e614e32a4609f756b57226c48e988c24e6
Agustina Magnalardo <agusmagna@MacBook-Pro-de-AgustinaMagna.local>
2019-08-13 00:27:19 -0300

    Mediaquerys

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -34,7 +34,7 @@
                 </a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                    <span><i class="fas fa-bars"></i></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -57,7 +57,7 @@
                       <a href="{{ url('/panelAdmin')}}" class='panelAdminLink'>Ir al panel de edición</li>
                     @endif
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto loginRegister">
                         <!-- Authentication Links -->
                         @guest
                             <li>
