a64bb90dda72a5d8bca8e470c598d6e9f508ee5b
Agustina Magnalardo <agusmagna@MacBook-Pro-de-AgustinaMagna.local>
2019-08-21 15:31:24 -0300

    Cambio en el javascript de admin

--- public/js/panelAdmin.js
+++ public/js/panelAdmin.js
@@ -8,10 +8,23 @@ machines.addEventListener('click', function(e){
     e.preventDefault();
     document.querySelector('#tableMachines').style.display = 'block';
     document.querySelector('#tableCapsules').style.display = 'none';
+    window.name = 'panelAdminMaquinas'
   })
 capsules.addEventListener('click', function(e){
       e.preventDefault();
       document.querySelector('#tableMachines').style.display = 'none';
       document.querySelector('#tableCapsules').style.display = 'block';
+      window.name = 'panelAdminCapsulas'
     })
+}
+
+if(window.name == 'panelAdminMaquinas'){
+
+  document.querySelector('#tableMachines').style.display = 'block';
+  document.querySelector('#tableCapsules').style.display = 'none';
+}
+if(window.name == 'panelAdminCapsulas'){
+
+  document.querySelector('#tableMachines').style.display = 'none';
+  document.querySelector('#tableCapsules').style.display = 'block';
 }
